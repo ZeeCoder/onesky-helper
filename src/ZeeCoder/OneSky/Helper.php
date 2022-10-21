1145233d31d76b0869c00f9aa8e9818172af0681
Igor Santos <532299+igorsantos07@users.noreply.github.com>
2022-10-21 00:13:14 -0300

    Add upload command with file type auto-discovery

--- src/ZeeCoder/OneSky/Helper.php
+++ src/ZeeCoder/OneSky/Helper.php
@@ -104,4 +104,63 @@ class Helper
             ]
         );
     }
+
+	/**
+	 * Sends a file for further processing
+	 * @param string $filePath
+	 * @param ?string $locale A locale in the format pt-BR, or null if this is the base language file
+	 * @param string $format Any of the formats defined by OneSky (see docs below). If not given, it's inferred from the extension - or raises an extension if that's not possible. Pay attention: some extensions share different formats supported by OneSky, such as .php holding PHP, PHP_SHORT_ARRAY or PHP_VARIABLES. Refer to OneSky docs to make sure you're using the right format for your use-case.
+	 * @param bool $keepStrings If false, strings missing from the same filename previously uploaded get deprecated.
+	 * @param bool $allowSameAsOriginal If false, skips importing translations with the same content as their key.
+	 * @see https://github.com/onesky/api-documentation-platform/blob/master/reference/format.md
+	 * @see https://github.com/onesky/api-documentation-platform/blob/master/resources/file.md#upload---upload-a-file
+	 * @return mixed
+	 */
+	public function uploadFile($filePath, $locale = null, $format = null, $keepStrings = true, $allowSameAsOriginal = false)
+	{
+		if (!$format) {
+			$ext = strtoupper(substr($filePath, strrpos($filePath, '.') + 1));
+			switch ($ext) {
+				case 'PO':
+					$format = 'GNU_PO';
+					break;
+				case 'POT':
+					$format = 'GNU_POT';
+					break;
+				case 'JSON':
+					$format = 'HIERARCHICAL_JSON';
+					break;
+				case 'PROPERTIES':
+					$format = 'JAVA_PROPERTIES';
+					break;
+				case 'HTML':
+				case 'INI':
+				case 'PHP':
+				case 'RESJSON':
+				case 'RESW':
+				case 'RRC':
+				case 'TMX':
+				case 'XLIFF':
+				case 'YML':
+				case 'YAML':
+					$format = $ext;
+					break;
+			}
+			if (!$format) {
+				throw new \InvalidArgumentException("Cannot infer file format for $filePath");
+			}
+		}
+
+		return $this->client->files(
+			'upload',
+			[
+				'project_id' => $this->config['project_id'],
+				'file' => $filePath,
+				'file_format' => $format,
+				'locale' => $locale,
+				'is_keeping_all_strings' => $keepStrings,
+				'is_allow_translation_same_as_original' => $allowSameAsOriginal
+			]
+		);
+	}
 }
